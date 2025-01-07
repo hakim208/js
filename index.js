41048fb11ab82fb4a576adb7593361af0f434275
hakim208 <inoyatovhakim5@gmail.com>
2025-01-07 12:05:39 +0300

    number 2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var a = 8
-var text = "The next number for the number "
-var text1 = "is"
-console.log(text,a,text1,a+1);
+// var a = 8
+// var text = "The next number for the number "
+// var text1 = "is"
+// console.log(text,a,text1,a+1);
 
-
+var number1 = 60;
+var number2 = 5
+console.log(number1/number2);
